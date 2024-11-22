2085f31556c38ecdfffe0c6012f0a2a4217f24e1
Jupet <animasjupetsamson@gmail.com>
2024-11-21 18:29:12 -0800

    update

--- public/js/today-forecast.js
+++ public/js/today-forecast.js
@@ -11,98 +11,62 @@ function getTodayForecast() {
           text: "sunny",
           code: 1000,
         },
-        temp_c: 6.6,
+        temp_c: -1.0,
       },
       {
         time: 10,
         weather: {
-          text: "sunny",
-          code: 1000,
+          text: "rain",
+          code: 1195,
         },
-        temp_c: 6.6,
+        temp_c: 1.0,
       },
       {
         time: 11,
         weather: {
-          text: "sunny",
-          code: 1000,
+          text: "thunderstorms-snow",
+          code: 1282,
         },
-        temp_c: 6.6,
+        temp_c: 2.0,
       },
       {
         time: 12,
         weather: {
-          text: "sunny",
-          code: 1000,
+          text: "partly-cloudy-day-snow",
+          code: 1222,
         },
-        temp_c: 6.6,
+        temp_c: 3.0,
       },
       {
         time: 13,
         weather: {
-          text: "sunny",
-          code: 1000,
+          text: "partly-cloudy-day-rain",
+          code: 1063,
         },
-        temp_c: 6.6,
+        temp_c: 4.0,
       },
       {
         time: 14,
         weather: {
-          text: "sunny",
-          code: 1000,
+          text: "partly-cloudy-day-sleet",
+          code: 1249,
         },
-        temp_c: 6.6,
+        temp_c: 4.0,
       },
       {
         time: 15,
         weather: {
-          text: "sunny",
-          code: 1000,
+          text: "snow",
+          code: 1114,
         },
-        temp_c: 6.6,
+        temp_c: 1.0,
       },
     ],
   };
 }
 
 // replace forecast data with getTodayForecast().forecast
-const forecast = [
-  {
-    hour: 9,
-    weather: "rainy",
-    temperature: 9,
-  },
-  {
-    hour: 10,
-    weather: "cloudy",
-    temperature: 4,
-  },
-  {
-    hour: 11,
-    weather: "sunny",
-    temperature: 3,
-  },
-  {
-    hour: 12,
-    weather: "windy",
-    temperature: 4,
-  },
-  {
-    hour: 13,
-    weather: "cold",
-    temperature: 7,
-  },
-  {
-    hour: 14,
-    weather: "storm",
-    temperature: 1,
-  },
-  {
-    hour: 15,
-    weather: "storm",
-    temperature: 2,
-  },
-];
+const forecast = getTodayForecast().forecast;
 
 const icons = {
   sunny: "/icons/weather/clear-day.svg",
@@ -116,59 +80,59 @@ const icons = {
 // modify following code with new forecast data structure
 todayForecastBox.innerHTML = `
     <div class="hour">
-        <div class="time">${forecast[0].hour}:00</div>
+        <div class="time">${forecast[0].time}:00</div>
         <img src="${
           // change getWeatherIcon(forecast[0].weather.code)
-          icons[forecast[0].weather]
+          getWeatherIcon(forecast[0].weather.code)
         }" alt="Sunny" width="400" height="300">
-        <div class="temperature">${forecast[0].temperature}°</div>
+        <div class="temperature">${forecast[0].temp_c}°</div>
     </div>
 
     <div class="hour">
-        <div class="time">${forecast[1].hour}:00</div>
-        <img src="${
-          icons[forecast[1].weather]
-        }" alt="Sunny" width="400" height="300">
-        <div class="temperature">${forecast[1].temperature}°</div>
+        <div class="time">${forecast[1].time}:00</div>
+        <img src="${getWeatherIcon(
+          forecast[1].weather.code
+        )}" alt="Sunny" width="400" height="300">
+        <div class="temperature">${forecast[1].temp_c}°</div>
     </div>
 
     <div class="hour">
-        <div class="time">${forecast[2].hour}:00</div>
-        <img src="${
-          icons[forecast[2].weather]
-        }" alt="Sunny" width="400" height="300">
-        <div class="temperature">${forecast[2].temperature}°</div>
+        <div class="time">${forecast[2].time}:00</div>
+        <img src="${getWeatherIcon(
+          forecast[2].weather.code
+        )}" alt="Sunny" width="400" height="300">
+        <div class="temperature">${forecast[2].temp_c}°</div>
     </div>
 
     <div class="hour">
-        <div class="time">${forecast[3].hour}:00</div>
-        <img src="${
-          icons[forecast[3].weather]
-        }" alt="Sunny" width="400" height="300">
-        <div class="temperature">${forecast[3].temperature}°</div>
+        <div class="time">${forecast[3].time}:00</div>
+        <img src="${getWeatherIcon(
+          forecast[3].weather.code
+        )}" alt="Sunny" width="400" height="300">
+        <div class="temperature">${forecast[3].temp_c}°</div>
     </div>
 
     <div class="hour">
-        <div class="time">${forecast[4].hour}:00</div>
-        <img src="${
-          icons[forecast[4].weather]
-        }" alt="Sunny" width="400" height="300">
-        <div class="temperature">${forecast[4].temperature}°</div>
+        <div class="time">${forecast[4].time}:00</div>
+        <img src="${getWeatherIcon(
+          forecast[4].weather.code
+        )}" alt="Sunny" width="400" height="300">
+        <div class="temperature">${forecast[4].temp_c}°</div>
     </div>
 
     <div class="hour">
-        <div class="time">${forecast[5].hour}:00</div>
-        <img src="${
-          icons[forecast[5].weather]
-        }" alt="Sunny" width="400" height="300">
-        <div class="temperature">${forecast[5].temperature}°</div>
+        <div class="time">${forecast[5].time}:00</div>
+        <img src="${getWeatherIcon(
+          forecast[5].weather.code
+        )}" alt="Sunny" width="400" height="300">
+        <div class="temperature">${forecast[5].temp_c}°</div>
     </div>
 
     <div class="hour">
-        <div class="time">${forecast[6].hour}:00</div>
-        <img src="${
-          icons[forecast[6].weather]
-        }" alt="Sunny" width="400" height="300">
-        <div class="temperature">${forecast[6].temperature}°</div>
+        <div class="time">${forecast[6].time}:00</div>
+        <img src="${getWeatherIcon(
+          forecast[6].weather.code
+        )}" alt="Sunny" width="400" height="300">
+        <div class="temperature">${forecast[6].temp_c}°</div>
     </div>
 `;
